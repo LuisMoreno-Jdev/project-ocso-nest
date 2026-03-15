826ef1b8d313f99f1ffee82087bc15deb8f0cb84
LuisMoreno-Jdev <luiscominito2006@gmail.com>
2026-03-14 18:58:02 -0600

    initial config for locations route

--- src/locations/dto/create-location.dto.ts
+++ src/locations/dto/create-location.dto.ts
@@ -1,1 +1,13 @@
-export class CreateLocationDto {}
+import { ArrayNotEmpty, IsArray, IsString, MaxLength } from "class-validator";
+
+export class CreateLocationDto {
+    @IsString()
+    @MaxLength(35)
+    locationName: string;
+    @IsString()
+    @MaxLength(160)
+    locationAddress: string;
+    @IsArray()
+    @ArrayNotEmpty()
+    locationLatLng: number[];
+}
--- src/locations/locations.service.ts
+++ src/locations/locations.service.ts
@@ -1,23 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
 import { CreateLocationDto } from './dto/create-location.dto';
 import { UpdateLocationDto } from './dto/update-location.dto';
+import { Location } from './entities/location.entity';
 
 @Injectable()
 export class LocationsService {
-  create(createLocationDto: CreateLocationDto) {
-    return 'This action adds a new location';
+  constructor(
+    private locationsRepository: Repository<Location>
+  ){}
+  create(createLocationDto: CreateLocationDto){
+    return this.locationsRepository.save(createLocationDto);
   }
 
   findAll() {
-    return `This action returns all locations`;
+    return this.locationsRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} location`;
+    const location = this.locationsRepository.findOneBy({
+      locationId: id,
+    })
+    if(!location) throw new NotFoundException('Location not found');
+    return location;
   }
 
   update(id: number, updateLocationDto: UpdateLocationDto) {
-    return `This action updates a #${id} location`;
+    const location = this.locationsRepository.preload({
+      locationId: id,
+      ...updateLocationDto,
+    })
+    return location;
   }
 
   remove(id: number) {
