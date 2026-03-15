8691937b3a125e0837b51191ec56bae947899d00
LuisMoreno-Jdev <luiscominito2006@gmail.com>
2026-03-14 18:39:57 -0600

    Created three new resources to expand backend capabilities

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,9 @@ import { AppService } from './app.service';
 import { EmployeesModule } from './employees/employees.module';
 import { ProductsModule } from './products/products.module';
 import { ProvidersModule } from './providers/providers.module';
+import { ManagersModule } from './managers/managers.module';
+import { LocationsModule } from './locations/locations.module';
+import { RegionsModule } from './regions/regions.module';
 
 @Module({
   imports: [
@@ -22,7 +25,7 @@ import { ProvidersModule } from './providers/providers.module';
     }),
     EmployeesModule, 
     ProductsModule, 
-    ProvidersModule,
+    ProvidersModule, ManagersModule, LocationsModule, RegionsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
